d3aafa749e3ee7a1ed5347faa26e268fcd23dd6b
Neil Swart <neilswart3@gmail.com>
2020-03-29 16:57:30 +0200

    added stats boxes

---
+++ src/components/LatestStats/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StatsBox = styled.div`
+  border: 1px solid #eee;
+  padding: 1rem;
+  text-align: center;
+`;
+
+const Styled = { StatsBox };
+
+export default Styled;
