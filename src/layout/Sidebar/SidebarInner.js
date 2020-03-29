7840ee3ecb070db157e247e2e758e397b7ac80ec
Bhekani Khumalo <bhekanik@gmail.com>
2020-03-29 17:57:18 +0100

    add twitter, facebook, google and phone number login

--- src/layout/Sidebar/SidebarInner.js
+++ src/layout/Sidebar/SidebarInner.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-
+import { connect } from 'react-redux';
+import { actions } from 'src/store/definitions/auth';
 import mainNavLinks from 'src/layout/nav/main';
 import secondaryNavLinks from 'src/layout/nav/secondary';
 import generateLinks from 'src/layout/nav/generateLinks';
 import IsProtectedPage from 'src/app/lib/firebase/auth/IsProtectedPage';
+import { Button } from '@material-ui/core';
 
-const SidebarInner = () => {
+const SidebarInner = props => {
   const router = useRouter();
+
+  const handleSignOut = () => {
+    const { signOut } = props;
+    signOut();
+  };
 
   return (
     <>
       <div className='branding'>
         <h3>C19 Se Push</h3>
       </div>
-      <div className='main-nav'>{generateLinks(mainNavLinks, router.asPath)}</div>
-      <div className='secondary-nav'>{generateLinks(secondaryNavLinks, router.asPath)}</div>
+      <div className='main-nav'>
+        {generateLinks(mainNavLinks, router.asPath)}
+      </div>
+      <div className='secondary-nav'>
+        {generateLinks(secondaryNavLinks, router.asPath)}
+      </div>
+      <div className='sign-out'>
+        <Button variant='outlined' color='primary' onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      </div>
     </>
   );
 };
 
-export default IsProtectedPage(SidebarInner);
+const mapDispatchToProps = dispatch => ({
+  signOut: () => dispatch(actions.signOutRequested()),
+});
+
+export default connect(null, mapDispatchToProps)(IsProtectedPage(SidebarInner));
