85074b9c2268052e74c7c11235f4cef2c0a0da47
pushplaybang <pushplaybang@gmail.com>
2020-03-31 02:02:21 +0200

    prettier

--- src/components/Survey/fragments/OutcomeContent.js
+++ src/components/Survey/fragments/OutcomeContent.js
@@ -7,13 +7,46 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 import { actions } from 'src/store/definitions/survey';
 
-const OutcomesContent =({ active, outcome = {}, setOutcome }) => {
+const OutcomesContent = ({ active, outcome = {}, setOutcome }) => {
   if (!active) return null;
   setOutcome(outcome);
   const whatsNext = {
-    severe: () => (<><Link href='/content/[page]'  as='/content/where-to-help'><Button variant='contained' color='primary' endIcon={<ArrowRightIcon />}>Where To Get Help</Button></Link></>),
-    warn: () => (<><Link href='/content/[page]' as='/content/general-guidelines'><Button variant='contained' color='primary'endIcon={<ArrowRightIcon />}>General Guidelines</Button></Link></>),
-    normal: () => (<><Link href='/latest-stats' as='/latest-stats'><Button variant='contained' color='primary' endIcon={<ArrowRightIcon />}>View Latest Stats</Button></Link></>),
+    severe: () => (
+      <>
+        <Link href='/content/[page]' as='/content/where-to-help'>
+          <Button
+            variant='contained'
+            color='primary'
+            endIcon={<ArrowRightIcon />}>
+            Where To Get Help
+          </Button>
+        </Link>
+      </>
+    ),
+    warn: () => (
+      <>
+        <Link href='/content/[page]' as='/content/general-guidelines'>
+          <Button
+            variant='contained'
+            color='primary'
+            endIcon={<ArrowRightIcon />}>
+            General Guidelines
+          </Button>
+        </Link>
+      </>
+    ),
+    normal: () => (
+      <>
+        <Link href='/latest-stats' as='/latest-stats'>
+          <Button
+            variant='contained'
+            color='primary'
+            endIcon={<ArrowRightIcon />}>
+            View Latest Stats
+          </Button>
+        </Link>
+      </>
+    ),
   };
 
   return (
@@ -24,25 +57,19 @@ const OutcomesContent =({ active, outcome = {}, setOutcome }) => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           key='results'>
-        <div className="results">
-          <Typography variant='h1' className={outcome.severity}>
-            <strong>{outcome.title}</strong>
-          </Typography>
-          <Typography variant='body1'>
-            {outcome.body}
-          </Typography>
-          <Typography variant='h4'>should you test?</Typography>
-          <Typography variant='body1'>
-           { outcome.testStatus}
-          </Typography>
-          <div className="next">
-            {whatsNext[outcome.severity]()}
+          <div className='results'>
+            <Typography variant='h1' className={outcome.severity}>
+              <strong>{outcome.title}</strong>
+            </Typography>
+            <Typography variant='body1'>{outcome.body}</Typography>
+            <Typography variant='h4'>should you test?</Typography>
+            <Typography variant='body1'>{outcome.testStatus}</Typography>
+            <div className='next'>{whatsNext[outcome.severity]()}</div>
           </div>
-        </div>
         </motion.div>
       ) : null}
     </AnimatePresence>
-  )
+  );
 };
 
 const mapDispatch = dispatch => ({
