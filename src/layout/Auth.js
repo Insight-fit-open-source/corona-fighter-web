a58abce357f4f002566d81f8dbf2e49e03668345
pushplaybang <pushplaybang@gmail.com>
2020-03-31 19:14:07 +0200

    animated background

--- src/layout/Auth.js
+++ src/layout/Auth.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
+import VirusBg from 'src/components/common/VirusBg';
 import Styled from './styles';
 
 const AuthLayout = ({ children }) => (
   <Styled.Wrap>
     <Styled.AuthBody>{children}</Styled.AuthBody>
+    <VirusBg />
   </Styled.Wrap>
 );
 
