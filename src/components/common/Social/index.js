b92527e6ac8c371f729b42d7377375f49b3efd26
pushplaybang <pushplaybang@gmail.com>
2020-04-04 01:37:32 +0200

    fix dom structure for social icons

--- src/components/common/Social/index.js
+++ src/components/common/Social/index.js
@@ -17,45 +17,39 @@ import {
 import Styled from './style';
 
 export default ({ invert, left, right, hideOnMobile }) => (
-  <Styled.SocialWrapper invert={invert} left={left} right={right} hideOnMobile={hideOnMobile}>
+  <Styled.SocialWrapper
+    invert={invert}
+    left={left}
+    right={right}
+    hideOnMobile={hideOnMobile}>
     <EmailShareButton
       url='http://testforcovid.co.za'
       subject='Start your COVID-19 symptom assessment now'
       body='CoronaFighter is a tool and an initiative: a tool for you to track symptoms and an initiative to reduce pressure on medical facilities, reduce deaths and track the evolution of the disease in our country.'>
-      <IconButton>
-        <Email />
-      </IconButton>
+      <Email />
     </EmailShareButton>
     <FacebookShareButton
       url='http://testforcovid.co.za'
       quote='Start your COVID-19 symptom assessment now'
       hashtag='COVID19'>
-      <IconButton>
-        <Facebook />
-      </IconButton>
+      <Facebook />
     </FacebookShareButton>
     <LinkedinShareButton
       url='http://testforcovid.co.za'
       title='Start your COVID-19 symptom assessment now'
       summary='CoronaFighter is a tool and an initiative: a tool for you to track symptoms and an initiative to reduce pressure on medical facilities, reduce deaths and track the evolution of the disease in our country'
       source='Corona Fighter'>
-      <IconButton>
-        <LinkedIn />
-      </IconButton>
+      <LinkedIn />
     </LinkedinShareButton>
     <TwitterShareButton
       url='http://testforcovid.co.za'
       title='Start your COVID-19 symptom assessment now'>
-      <IconButton>
-        <Twitter />
-      </IconButton>
+      <Twitter />
     </TwitterShareButton>
     <WhatsappShareButton
       url='http://testforcovid.co.za'
       title='Start your COVID-19 symptom assessment now'>
-      <IconButton>
-        <WhatsApp />
-      </IconButton>
+      <WhatsApp />
     </WhatsappShareButton>
   </Styled.SocialWrapper>
 );
