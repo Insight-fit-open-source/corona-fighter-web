021a2d5ea8fc0a2515a5b04f1d0eab0f1909040b
Jacques Nieuwoudt <jacques@86degrees.com>
2020-05-27 13:34:11 +0200

    chore(app): updated survey result calc

--- src/components/Survey/config/calculateOutcome.js
+++ src/components/Survey/config/calculateOutcome.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 
 export default (selection, outcomes) => {
-  console.log('selection:', selection);
   const results = _.reduce(
     Object.keys(selection).map(key => {
       const result = { key };
       if (
         !_.isArray(selection[key]) ||
         selection[key].length < 1 ||
-        _.includes(selection[key], 'none')
+        _.includes(selection[key], 'none') ||
+        _.includes(selection[key], 'no')
       ) {
         result.flag = false;
         return result;
@@ -28,23 +28,14 @@ export default (selection, outcomes) => {
     {},
   );
 
-  console.log(results);
-  const clear =
-    !results['severe-symptoms'] &&
-    !results['typical-symptoms'] &&
-    !results['atypical-symptoms'];
+  const severeSymptoms =
+    results.breathlessness ||
+    results.cough ||
+    results.breathlessness ||
+    results.breathlessness;
+  const atypicalSymptoms = results['atypical-symptoms'];
 
-  // FIXME: This seems like a bad approach.
-  // console.log(results);
-  if (results['severe-symptoms']) return outcomes.outcome1;
-  if (results['typical-symptoms'] && results['travel-contact'])
-    return outcomes.outcome2;
-  if (clear && !results['travel-contact']) return outcomes.outcome3;
-  if (clear && results['travel-contact']) return outcomes.outcome4;
-  if (results['typical-symptoms'] && !results['travel-contact'])
-    return outcomes.outcome7;
-  if (results['atypical-symptoms'] && !results['travel-contact'])
-    return outcomes.outcome5;
-  if (results['atypical-symptoms'] && results['travel-contact'])
-    return outcomes.outcome6;
+  if (severeSymptoms) return outcomes.severeOutcome;
+  if (atypicalSymptoms) return outcomes.warningOutcome;
+  return outcomes.normalOutcome;
 };
