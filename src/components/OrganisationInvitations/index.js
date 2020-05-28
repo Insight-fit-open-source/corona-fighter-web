d11ff69a8286f30948b50db4a3f92836caa7275d
Reiner <reiner@86degrees.com>
2020-05-28 13:06:39 +0200

    simple modal

--- src/components/OrganisationInvitations/index.js
+++ src/components/OrganisationInvitations/index.js
@@ -3,8 +3,10 @@ import {
   ExpansionPanel,
   ExpansionPanelDetails,
   ExpansionPanelSummary,
+  Modal,
   Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import SubmitIcon from '@material-ui/icons/ChevronRight';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import _ from 'lodash';
@@ -13,6 +15,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { actions } from 'src/store/definitions/survey';
 import { Item, Wrapper } from './styles';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export const OrganisationInvitations = ({
   requestSync,
@@ -23,9 +51,28 @@ export const OrganisationInvitations = ({
     requestSync();
   }, [requestSync]);
 
-  const openModal = () => {
-    // open modal
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
   };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id='simple-modal-title'>Text in a modal</h2>
+      <p id='simple-modal-description'>
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+      {/* <SimpleModal /> */}
+    </div>
+  );
 
   return (
     <Wrapper>
@@ -34,10 +81,18 @@ export const OrganisationInvitations = ({
         variant='contained'
         color='primary'
         endIcon={<SubmitIcon />}
-        onClick={openModal}
+        onClick={handleOpen}
         className='invite-button'>
         Invite a user
       </Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'>
+        {body}
+      </Modal>
 
       {surveyResultsCount
         ? _(surveyResults)
